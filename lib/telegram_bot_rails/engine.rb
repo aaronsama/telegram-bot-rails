9449a6a45e131810adeaaa5388280cc4b9cd64b0
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2016-04-27 19:42:02 +0200

    added rspec and removed testunit

--- lib/telegram_bot_rails/engine.rb
+++ lib/telegram_bot_rails/engine.rb
@@ -1,5 +1,12 @@
 module TelegramBotRails
   class Engine < ::Rails::Engine
     isolate_namespace TelegramBotRails
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
--- test/models/telegram_bot_rails/conversation_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module TelegramBotRails
-  class ConversationTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
